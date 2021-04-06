85f7a9a50a939b2c7ce216c58cc95dc507f33e83
Xmuluneh <71099880+Xmuluneh@users.noreply.github.com>
2021-04-06 12:31:32 +0300

    Update Solution.java

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -3,23 +3,30 @@ package com.company;
 public class Solution {
     public static boolean validatePin(String pin) {
         int count=0;
-        char Char;
+       
         for(int i=0;i<pin.length();i++){
-            Char=pin.charAt(i);
-            if (!Char.isDigit(Char)) {
-                continue;
+           
+            if (Character.isDigit(pin.charAt(i)) {
+               count++
             }
-            count++;
+           
 
         }
+       if(count==4||count==6){
+          return true;
+       }else{
 
-
-
-
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
+         System.out.println(validatePin("12345"));
+        System.out.println(validatePin("q123"));
+        System.out.println(validatePin("123456"));
+        System.out.println(validatePin("a12345"));
+
+    }
+}
 
     }
 }
